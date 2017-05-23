b0221c02d9581a38e810770ec925c4b97d64074a
kai0511 <zhaokai09@gmail.com>
2017-05-23 18:09:44 +0800

    Update shell_cmds.sh

--- shell_cmds.sh
+++ shell_cmds.sh
@@ -1,2 +1,5 @@
  # download all pdf files in 1 depth from the link and put them directly under current folder
  wget -A pdf -r -l 1 -nd http://people.eecs.berkeley.edu/~jordan/prelims/  
+ 
+ # Continue getting a partially-downloaded file
+ wget -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE92nnn/GSE92742/suppl/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx.gz
