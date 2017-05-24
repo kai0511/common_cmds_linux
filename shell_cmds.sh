de0ecc84abfae2dc1b779a5f3a778a489f495e2f
kai0511 <zhaokai09@gmail.com>
2017-05-24 15:53:33 +0800

    Update shell_cmds.sh

--- shell_cmds.sh
+++ shell_cmds.sh
@@ -5,4 +5,12 @@
  wget -c ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE92nnn/GSE92742/suppl/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx.gz
  
  # merge mutiple pdf files into one, the last one is the output file.
-  pdfunite chapter2.pdf chapter3.pdf itpm.pdf
+ pdfunite chapter2.pdf chapter3.pdf itpm.pdf
+ 
+ # sort multiple columns of a file
+ # -t the separator of different columns
+ # -k sort according which column
+ # -nr sort numerically and inversely
+ sort -t',' -k2 -nr svm_result.out | awk -F',' '$3==1{print $0}'  # print the row whose third column is 1
+ 
+ 
